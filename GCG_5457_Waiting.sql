f07e44ecc7cb1ea6b312c07b14ba5b04ddf6dab7
Service Web <sw@gssmail.com>
2024-03-14 11:23:38 -0500

    Commit changes in C:\Websites\Git\emarlow\5457\GCG_5457_Waiting.sql

---
+++ GCG_5457_Waiting.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "GCG_5457_Waiting" (
+ "Batch" CHAR(5),
+ "DateToPost" DATE,
+ "DateStart" DATE,
+ "DateEnd" DATE,
+ "Gains" NUMERIC(16,2),
+ "Losses" NUMERIC(16,2),
+ "ReAllocateAmount" NUMERIC(16,2) );
